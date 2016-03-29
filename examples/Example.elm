2a3fe6ce3798184cb4e9444ff6846fadd30eb963
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-28 21:46:51 -0700

    Add an example.

---
+++ examples/Example.elm
@@ -0,0 +1,19 @@
+module Example (..) where
+
+import Json.Decode exposing (int, string, float, Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcode)
+
+
+type alias User =
+  { id : Int
+  , name : String
+  , percentExcited : Float
+  }
+
+
+userDecoder : Decoder User
+userDecoder =
+  decode User
+    |> required "id" int
+    |> optional "name" string "(fallback if name not present)"
+    |> hardcode 1.0
