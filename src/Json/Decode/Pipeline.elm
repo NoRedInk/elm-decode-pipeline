a115967ca8d50a1d8473ee6a76705e409fbc1dcc
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-28 20:53:18 -0700

    Initial import.

---
+++ src/Json/Decode/Pipeline.elm
@@ -0,0 +1,48 @@
+module Json.Decode.Pipeline (required, optional, fromResult, decode, hardcode, delegate) where
+
+{-| # elm-decode-pipeline
+
+@docs required, optional, hardcode, delegate, fromResult, decode
+-}
+
+import Json.Decode exposing (..)
+
+
+{-| -}
+required : String -> Decoder a -> Decoder (a -> b) -> Decoder b
+required key valDecoder decoder =
+  andThen decoder (\wrappedFn -> map wrappedFn (key := valDecoder))
+
+
+{-| -}
+optional : String -> Decoder a -> a -> Decoder (a -> b) -> Decoder b
+optional key valDecoder fallback decoder =
+  let
+    maybeDecoder =
+      maybe (key := valDecoder) `andThen` ((Maybe.withDefault fallback) >> succeed)
+  in
+    andThen decoder (\wrappedFn -> map wrappedFn maybeDecoder)
+
+
+{-| -}
+hardcode : a -> Decoder (a -> b) -> Decoder b
+hardcode val decoder =
+  andThen decoder (\wrappedFn -> map wrappedFn (succeed val))
+
+
+{-| -}
+delegate : Decoder a -> Decoder (a -> b) -> Decoder b
+delegate delegated decoder =
+  andThen decoder (\wrappedFn -> map wrappedFn delegated)
+
+
+{-| -}
+fromResult : Decoder (Result String a) -> Decoder a
+fromResult resultDecoder =
+  andThen resultDecoder (\result -> customDecoder (succeed ()) (\_ -> result))
+
+
+{-| -}
+decode : a -> Decoder a
+decode =
+  succeed
