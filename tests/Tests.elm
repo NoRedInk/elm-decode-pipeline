28ccf842225d45f10e92360fef8add10274318f0
Richard Feldman <richard.t.feldman@gmail.com>
2016-08-11 16:39:56 -0700

    Refactor tests to use idiomatic elm-test style.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,14 +1,25 @@
 module Tests exposing (..)
 
 import Test exposing (..)
-import Expect
-import Json.Decode.Pipeline as Pipeline
-import Json.Decode as Json
+import Expect exposing (Expectation)
+import Json.Decode.Pipeline
+    exposing
+        ( decode
+        , required
+        , requiredAt
+        , optional
+        , optionalAt
+        , resolveResult
+        )
+import Json.Decode exposing (Decoder, string, null)
 
 
-decode : String -> Json.Decoder a -> Result String a
-decode =
-    flip Json.decodeString
+{-| Run some JSON through a Decoder and return the result.
+-}
+runWith : String -> Decoder a -> Result String a
+runWith =
+    flip Json.Decode.decodeString
+
 
 isError : Result err ok -> Bool
 isError result =
@@ -19,83 +30,85 @@ isError result =
         Ok _ ->
             False
 
-expectErr : Result err ok -> Expect.Expectation
+
+expectErr : Result err ok -> Expectation
 expectErr result =
     isError result
         |> Expect.true ("Expected an Err but got " ++ toString result)
+
 
 all : Test
 all =
     describe
         "Json.Decode.Pipeline"
-        [ Pipeline.decode (,)
-            |> Pipeline.required "a" Json.string
-            |> Pipeline.required "b" Json.string
-            |> decode """{"a":"foo","b":"bar"}"""
-            |> Expect.equal (Ok ( "foo", "bar" ))
-            |> always
-            |> test "should decode basic example"
-        , Pipeline.decode (,)
-            |> Pipeline.requiredAt [ "a" ] Json.string
-            |> Pipeline.requiredAt [ "b", "c" ] Json.string
-            |> decode """{"a":"foo","b":{"c":"bar"}}"""
-            |> Expect.equal (Ok ( "foo", "bar" ))
-            |> always
-            |> test "should decode requiredAt fields"
-        , Pipeline.decode (,)
-            |> Pipeline.optionalAt [ "a", "b" ] Json.string "--"
-            |> Pipeline.optionalAt [ "x", "y" ] Json.string "--"
-            |> decode """{"a":{},"x":{"y":"bar"}}"""
-            |> Expect.equal (Ok ( "--", "bar" ))
-            |> always
-            |> test "should decode optionalAt fields"
-        , Pipeline.decode (,)
-            |> Pipeline.optional "a" Json.string "--"
-            |> Pipeline.optional "x" Json.string "--"
-            |> decode """{"x":"five"}"""
-            |> Expect.equal (Ok ( "--", "five" ))
-            |> always
-            |> test "optional succeeds if the field is not present"
-        , Pipeline.decode (,)
-            |> Pipeline.optional "a" Json.string "--"
-            |> Pipeline.optional "x" Json.string "--"
-            |> decode """{"a":null,"x":"five"}"""
-            |> Expect.equal (Ok ( "--", "five" ))
-            |> always
-            |> test "optional succeeds with fallback if the field is present but null"
-        , Pipeline.decode (,)
-            |> Pipeline.optional "a" (Json.null "null") "--"
-            |> Pipeline.optional "x" Json.string "--"
-            |> decode """{"a":null,"x":"five"}"""
-            |> Expect.equal (Ok ( "null", "five" ))
-            |> always
-            |> test "optional succeeds with result of the given decoder if the field is null and the decoder decodes nulls"
-        , Pipeline.decode (,)
-            |> Pipeline.optional "a" Json.string "--"
-            |> Pipeline.optional "x" Json.string "--"
-            |> decode """{"x":5}"""
-            |> expectErr
-            |> always
-            |> test "optional fails if the field is present but doesn't decode"
-        , Pipeline.decode (,)
-            |> Pipeline.optionalAt [ "a", "b" ] Json.string "--"
-            |> Pipeline.optionalAt [ "x", "y" ] Json.string "--"
-            |> decode """{"a":{},"x":{"y":5}}"""
-            |> expectErr
-            |> always
-            |> test "optionalAt fails if the field is present but doesn't decode"
-        , Pipeline.decode Err
-            |> Pipeline.required "error" Json.string
-            |> Pipeline.resolveResult
-            |> decode """{"error":"invalid"}"""
-            |> expectErr
-            |> always
-            |> test "resolveResult bubbles up decoded Err results"
-        , Pipeline.decode Ok
-            |> Pipeline.required "ok" Json.string
-            |> Pipeline.resolveResult
-            |> decode """{"ok":"valid"}"""
-            |> Expect.equal (Ok "valid")
-            |> always
-            |> test "resolveResult bubbles up decoded Ok results"
+        [ test "should decode basic example" <|
+            \() ->
+                decode (,)
+                    |> required "a" string
+                    |> required "b" string
+                    |> runWith """{"a":"foo","b":"bar"}"""
+                    |> Expect.equal (Ok ( "foo", "bar" ))
+        , test "should decode requiredAt fields" <|
+            \() ->
+                decode (,)
+                    |> requiredAt [ "a" ] string
+                    |> requiredAt [ "b", "c" ] string
+                    |> runWith """{"a":"foo","b":{"c":"bar"}}"""
+                    |> Expect.equal (Ok ( "foo", "bar" ))
+        , test "should decode optionalAt fields" <|
+            \() ->
+                decode (,)
+                    |> optionalAt [ "a", "b" ] string "--"
+                    |> optionalAt [ "x", "y" ] string "--"
+                    |> runWith """{"a":{},"x":{"y":"bar"}}"""
+                    |> Expect.equal (Ok ( "--", "bar" ))
+        , test "optional succeeds if the field is not present" <|
+            \() ->
+                decode (,)
+                    |> optional "a" string "--"
+                    |> optional "x" string "--"
+                    |> runWith """{"x":"five"}"""
+                    |> Expect.equal (Ok ( "--", "five" ))
+        , test "optional succeeds with fallback if the field is present but null" <|
+            \() ->
+                decode (,)
+                    |> optional "a" string "--"
+                    |> optional "x" string "--"
+                    |> runWith """{"a":null,"x":"five"}"""
+                    |> Expect.equal (Ok ( "--", "five" ))
+        , test "optional succeeds with result of the given decoder if the field is null and the decoder decodes nulls" <|
+            \() ->
+                decode (,)
+                    |> optional "a" (null "null") "--"
+                    |> optional "x" string "--"
+                    |> runWith """{"a":null,"x":"five"}"""
+                    |> Expect.equal (Ok ( "null", "five" ))
+        , test "optional fails if the field is present but doesn't decode" <|
+            \() ->
+                decode (,)
+                    |> optional "a" string "--"
+                    |> optional "x" string "--"
+                    |> runWith """{"x":5}"""
+                    |> expectErr
+        , test "optionalAt fails if the field is present but doesn't decode" <|
+            \() ->
+                decode (,)
+                    |> optionalAt [ "a", "b" ] string "--"
+                    |> optionalAt [ "x", "y" ] string "--"
+                    |> runWith """{"a":{},"x":{"y":5}}"""
+                    |> expectErr
+        , test "resolveResult bubbles up decoded Err results" <|
+            \() ->
+                decode Err
+                    |> required "error" string
+                    |> resolveResult
+                    |> runWith """{"error":"invalid"}"""
+                    |> expectErr
+        , test "resolveResult bubbles up decoded Ok results" <|
+            \() ->
+                decode Ok
+                    |> required "ok" string
+                    |> resolveResult
+                    |> runWith """{"ok":"valid"}"""
+                    |> Expect.equal (Ok "valid")
         ]
